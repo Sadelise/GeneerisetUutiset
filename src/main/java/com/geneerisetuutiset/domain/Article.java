a801c65733bce00d850b74943a9da1cc22871e6a
Sadelise <sharhio@hotmail.com>
2017-11-27 19:27:37 +0200

    article update

--- src/main/java/com/geneerisetuutiset/domain/Article.java
+++ src/main/java/com/geneerisetuutiset/domain/Article.java
@@ -1,14 +1,14 @@
 package com.geneerisetuutiset.domain;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @NoArgsConstructor
