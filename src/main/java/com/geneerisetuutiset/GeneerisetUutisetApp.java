4237bd815563d2ac41ee73ff723a18b00cbb9f8b
Sadelise <sharhio@hotmail.com>
2017-11-27 17:01:02 +0200

    travis conf

--- src/main/java/com/geneerisetuutiset/GeneerisetUutisetApp.java
+++ src/main/java/com/geneerisetuutiset/GeneerisetUutisetApp.java
@@ -5,8 +5,14 @@
  */
 package com.geneerisetuutiset;
 
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,5 +29,16 @@ public class GeneerisetUutisetApp {
 
     public static void main(String[] args) {
         SpringApplication.run(GeneerisetUutisetApp.class, args);
+    }
+
+    @Configuration
+    @Profile("production")
+    public class ProductionProfile {
+
+        @Bean
+        @ConfigurationProperties(prefix = "spring.datasource")
+        public DataSource dataSource() {
+            return DataSourceBuilder.create().build();
+        }
     }
 }
