39e742ea6745aa71162c9b7c171907fae54ca7f7
Sadelise <sharhio@hotmail.com>
2017-11-29 18:23:12 +0200

    categories autocreated

--- src/main/java/com/geneerisetuutiset/GeneerisetUutisetApp.java
+++ src/main/java/com/geneerisetuutiset/GeneerisetUutisetApp.java
@@ -5,7 +5,11 @@
  */
 package com.geneerisetuutiset;
 
+import com.geneerisetuutiset.domain.Category;
+import com.geneerisetuutiset.repositories.CategoryRepository;
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -16,6 +20,9 @@ import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class GeneerisetUutisetApp {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(GeneerisetUutisetApp.class, args);
@@ -30,5 +37,27 @@ public class GeneerisetUutisetApp {
         public DataSource dataSource() {
             return DataSourceBuilder.create().build();
         }
+    }
+
+    @PostConstruct
+    public void categories() {
+        Category category1 = new Category();
+        Category category2 = new Category();
+        Category category3 = new Category();
+        Category category4 = new Category();
+        Category category5 = new Category();
+        Category category6 = new Category();
+        category1.setName("Vanhat");
+        category2.setName("Uudet");
+        category3.setName("Hyvät");
+        category4.setName("Huonot");
+        category5.setName("Jännät");
+        category6.setName("Tylsät");
+        categoryRepository.save(category1);
+        categoryRepository.save(category2);
+        categoryRepository.save(category3);
+        categoryRepository.save(category4);
+        categoryRepository.save(category5);
+        categoryRepository.save(category6);
     }
 }
