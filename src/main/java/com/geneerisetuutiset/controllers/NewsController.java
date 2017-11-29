2435e855f49460c3031e41f7170a2d2b495a38da
Sadelise <sharhio@hotmail.com>
2017-11-29 18:19:55 +0200

    Controller methods added

--- src/main/java/com/geneerisetuutiset/controllers/NewsController.java
+++ src/main/java/com/geneerisetuutiset/controllers/NewsController.java
@@ -11,9 +11,12 @@ import com.geneerisetuutiset.domain.Category;
 import com.geneerisetuutiset.repositories.ArticleRepository;
 import com.geneerisetuutiset.repositories.AuthorRepository;
 import com.geneerisetuutiset.repositories.CategoryRepository;
+import com.geneerisetuutiset.services.NewsEditingService;
 import java.io.IOException;
-import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,7 +25,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class NewsController {
@@ -33,29 +38,27 @@ public class NewsController {
     private AuthorRepository authorRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private NewsEditingService newsEditingService;
 
     @RequestMapping("/")
     public String home(Model model) {
-        Category category1 = new Category();
-        Category category2 = new Category();
-        Category category3 = new Category();
-        category1.setId(1L);
-        category2.setId(2L);
-        category3.setId(3L);
-        category1.setName("Uudet");
-        category2.setName("Vanhat");
-        category3.setName("Kotimaiset");
-        categoryRepository.save(category1);
-        categoryRepository.save(category2);
-        categoryRepository.save(category3);
-        model.addAttribute("news", this.articleRepository.findAll());
+        Pageable pageable = PageRequest.of(0, 5, Sort.Direction.DESC, "published");
+        model.addAttribute("news", this.articleRepository.findAll(pageable));
         model.addAttribute("categories", this.categoryRepository.findAll());
         return "index";
     }
 
     @GetMapping("/news/{id}")
     public String getArticle(Model model, @PathVariable Long id) {
-        model.addAttribute("article", articleRepository.getOne(id));
+        Article article = articleRepository.getOne(id);
+        article.setTimesRead(article.getTimesRead() + 1);
+        model.addAttribute("article", article);
+        model.addAttribute("authors", article.getAuthors());
+        Pageable pageablePub = PageRequest.of(0, Integer.MAX_VALUE, Sort.Direction.DESC, "published");
+        model.addAttribute("published", articleRepository.findAll(pageablePub));
+        Pageable pageableRead = PageRequest.of(0, Integer.MAX_VALUE, Sort.Direction.DESC, "timesRead");
+        model.addAttribute("timesRead", articleRepository.findAll(pageableRead));
         return "article";
     }
 
@@ -70,29 +73,13 @@ public class NewsController {
 
     @PostMapping("/news/new")
     public String postArticle(Model model,
-            String title, String ingress, String authorNames, String content, String categoryNames,
-            @Param("file") MultipartFile picture) throws IOException {
-        Article article = new Article();
-        if (picture != null && picture.getContentType().equals("image/jpeg")) {
-            article.setPicture(picture.getBytes());
+            String title, String ingress, String authorNames, String content, String[] categoryNames,
+            @Param("file") MultipartFile picture, RedirectAttributes redirectAttributes) throws IOException {
+        newsEditingService.addNew(title, ingress, authorNames, content, categoryNames, picture);
+        if (title == null || authorNames == null || content == null) {
+            redirectAttributes.addFlashAttribute("message", "Artikkelin lisäys epäonnistui. Artikkelilla täytyy olla ainakin otsikko, yksi kirjoittaja ja sisältöä.");
+            return "redirect:/control";
         }
-        String replaceNames = authorNames.replace(" ", "").toLowerCase();
-        String replaceCategories = categoryNames.replace(" ", "").toLowerCase();
-        String[] names = replaceNames.split(",");
-        String[] categories = replaceCategories.split(",");
-        //TO DO turn names into objects
-        for (String category : categories) {
-        }
-//        Author author = authorRepository.findByName(authorName);
-//        Category category = categoryRepository.findByName(categoryName);
-//        article.addAuthor(author);
-//        article.addCategory(category);
-        article.setContent(content);
-        article.setIngress(ingress);
-        article.setPublished(LocalDate.now());
-        article.setTitle(title);
-
-        articleRepository.save(article);
         return "redirect:/";
     }
 
@@ -101,5 +88,32 @@ public class NewsController {
         model.addAttribute("news", this.articleRepository.findAll());
         model.addAttribute("categories", this.categoryRepository.findAll());
         return "control";
+    }
+
+    @GetMapping(path = "/pictures/{id}", produces = "image/gif")
+    @ResponseBody
+    public byte[] get(@PathVariable Long id) {
+        return articleRepository.getOne(id).getPicture();
+    }
+
+    @GetMapping("/news/category/{name}")
+    public String getArticlesByCategory(Model model, @PathVariable String name) {
+        Category category = categoryRepository.getOneByName(name);
+        model.addAttribute("news", category.getArticles());
+        return "filtered";
+    }
+
+    @GetMapping("/news/filtered/published")
+    public String getArticlesByPublishingTime(Model model) {
+        Pageable pageable = PageRequest.of(0, Integer.MAX_VALUE, Sort.Direction.DESC, "published");
+        model.addAttribute("published", articleRepository.findAll(pageable));
+        return "filtered";
+    }
+
+    @GetMapping("/news/filtered/timesRead")
+    public String getArticlesByPopularity(Model model) {
+        Pageable pageable = PageRequest.of(0, Integer.MAX_VALUE, Sort.Direction.DESC, "timesRead");
+        model.addAttribute("timesRead", articleRepository.findAll(pageable));
+        return "filtered";
     }
 }
