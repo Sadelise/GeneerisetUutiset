d55b41abe6af6e900dacd11b7f7341a162365a62
Sadelise <sharhio@hotmail.com>
2017-12-10 16:19:08 +0200

    user and security config and views

---
+++ src/main/java/com/geneerisetuutiset/domain/Account.java
@@ -0,0 +1,31 @@
+package com.geneerisetuutiset.domain;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Account extends AbstractPersistable<Long> {
+
+    @Id
+    private Long id;
+    @NotEmpty
+    @Size(min = 3, max = 12)
+    private String username;
+    @NotEmpty
+    @Size(min = 8)
+    private String password;
+    @NotEmpty
+    @OneToMany
+    private List<Role> roles;
+}
---
+++ src/main/java/com/geneerisetuutiset/security/ProductionSecurityConfiguration.java
@@ -0,0 +1,73 @@
+package com.geneerisetuutiset.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Profile("production")
+@EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
+@Configuration
+@EnableWebSecurity
+public class ProductionSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Value("admin.password")
+    private String adminPassword;
+    @Value("user.password")
+    private String userPassword;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().sameOrigin();
+
+        http.authorizeRequests()
+                .antMatchers("/h2-console/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
+                .antMatchers(HttpMethod.POST, "/").authenticated()
+                .anyRequest().authenticated();
+        http.formLogin()
+                .permitAll();
+
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/news/{id}").permitAll()
+                .antMatchers("/pictures/{id}").permitAll()
+                .antMatchers("/news/category/{name}").permitAll()
+                .antMatchers("/news/filtered/{filterBy}/{title}").permitAll()
+                .antMatchers("/control").hasRole("ADMIN")
+                .antMatchers("/news/new").hasRole("ADMIN")
+                .antMatchers("/edit/{id}").hasRole("ADMIN")
+                .anyRequest().authenticated().and()
+                .formLogin().permitAll().and()
+                .logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+        auth.inMemoryAuthentication()
+                .withUser("jack").password(userPassword).roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("will").password(adminPassword).roles("ADMIN");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
